b99c1a85be00b980bc2886a720912aec829c3d4c
psanusha <118505694+psanusha@users.noreply.github.com>
2023-05-10 23:03:55 +0530

    folder updates

--- LAB 2/Lab2.java
+++ LAB 2/Lab2.java
@@ -1,96 +1,97 @@
 import java.util.*;
 
-class Lab2 {
-    public static void main(String[] args) {
+import java.util.Scanner;
+class LAB2{
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter number of names");
-        int n = scan.nextInt();
-        String[] name = new String[n];
-        int count = 0;
-        boolean found = false;
-        
-        int choose;
-        do {
-            System.out.println("Enter your choice");
-            System.out.println("1. Add a name");
-            System.out.println("2. Search for a name");
-            System.out.println("3. Remove a name");
-            System.out.println("4. Print all names");
-            System.out.println("5. Exit the program");
-            choose = scan.nextInt();
+        String[] arr = new String[1024];
+        int count=0;
+        int cont;
+        do{
+            System.out.println("Enter your choice:");
+            System.out.println("1-->To enter names");
+            System.out.println("2-->To search for a name");
+            System.out.println("3-->To remove a name");
+            System.out.println("4-->To display all the names");
+            int choice=scan.nextInt();
+            scan.nextLine();
+    
             
-            switch(choose) {
+            switch(choice){
                 case 1:
-                    System.out.println("Enter the name");
-                    name[count] = scan.next();
-                    count++;
-                    break;
+                System.out.println("Enter the name:");
+                String name = scan.nextLine();
+                boolean fnd=false;
+                for(int i=0;i<count;i++){
+                    if(name.equalsIgnoreCase(arr[i])){
+                        fnd = true;
+                        break;
+
+                    }
+
+                }
+                if(!fnd){
+                        arr[count]=name;
+                        count=count+1;
+
                     
+                }
+                break;
+
                 case 2:
-                    System.out.println("Enter the name you want to search");
-                    String search = scan.next();
-                    found = false;
-                    for(int i = 0; i < count; i++) {
-                        if(name[i].equalsIgnoreCase(search)) {
-                            System.out.println("The name is at index " + i);
-                            found = true;
-                        }
+                System.out.println("Enter the name you want to search:");
+                String search = scan.nextLine();
+                boolean found = false;
+                for(int i=0;i<count;i++){
+                    if(search.equalsIgnoreCase(arr[i])){
+                        System.out.println(search+"is at the index "+i);
+                        found=true;
+                        break;
                     }
-                    if(!found) {
-                        System.out.println("Name not found");
-                    }
-                    break;
                     
+                }
+                if(!found){
+                    System.out.println("name is not found");
+                }
+                break;
+
                 case 3:
-                    System.out.println("Enter the name you want to remove");
-                    String remove = scan.next();
-                    int index = -1;
-                    for(int i = 0; i < count; i++) {
-                        if(name[i].equalsIgnoreCase(remove)) {
-                            index = i;
-                            break;
-                        }
+                System.out.println("Enter the name you want to remove:");
+                String remove = scan.nextLine();
+                int index =-1;
+                for(int i =0;i<count;i++){
+                    if(remove.equalsIgnoreCase(arr[i])){
+                        index=i;
+                        break;
                     }
-                    if(index == -1) {
-                        System.out.println("Name not found");
-                    } else {
-                        for(int i = index; i < count - 1; i++) {
-                            name[i] = name[i + 1];
-                        }
-                        count--;
-                        System.out.println("Name removed successfully");
+                }
+                if(index==-1){
+                    System.out.println("not found");
+                }else{
+                    for(int j=index;j<count-1;j++){
+                        arr[j]=arr[j+1];
+
                     }
-                    break;
-                    
+                    arr[count-1]=null;
+                    count=count-1;
+                }
+                break;
+                
+
                 case 4:
-                    System.out.println("Printing all names");
-                    for(int i = 0; i < count; i++) {
-                        System.out.println(name[i]);
-                    }
-                    break;
-                    
-                case 5:
-                    System.out.println("Exiting the program");
-                    break;
-                    
-                default:
-                    System.out.println("Invalid choice");
-                    break;
+                for(int i =0;i<count;i++){
+                    System.out.println(arr[i]);
+
+                }
+                
+
+
             }
-        } while(choose != 5);
-        scan.close();
+            System.out.println("enter 1 if you wanna continue");
+            cont=scan.nextInt();
+        }while(cont==1);
     }
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
